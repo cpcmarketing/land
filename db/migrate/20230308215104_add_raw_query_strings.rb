cef937bdcf496cc72564df8bbac7ee0101b8786a
themoffatt <amoffatt@beyondfinance.com>
2023-03-08 16:08:43 -0600

    fix typo

--- db/migrate/20230308215104_add_raw_query_strings.rb
+++ db/migrate/20230308215104_add_raw_query_strings.rb
@@ -1,5 +1,5 @@
 class AddRawQueryStrings < ActiveRecord::Migration[7.0]
   def change
-    add_column :visits, :raw_query_strings, :text
+    add_column :visits, :raw_query_string, :text
   end
 end
