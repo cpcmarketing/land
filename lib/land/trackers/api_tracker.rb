1ff429dcf10be77e0b4280becc2711e07c2a1a53
Kyle Annen <kannen@cpcmarketing.com>
2025-07-07 09:47:09 -0700

    feat(app): Add rescues for ActiveRecord::RecordInvalid errors
    This happens on a visit_id/cookie_id primary key collision when using #find_or_initialize_by in a transaction

--- lib/land/trackers/api_tracker.rb
+++ lib/land/trackers/api_tracker.rb
@@ -34,7 +34,8 @@ module Land
 
         begin
           Cookie.find_or_create_by(cookie_id: @cookie_id)
-        rescue ActiveRecord::RecordNotUnique
+        rescue ActiveRecord::RecordInvalid,
+               ActiveRecord::RecordNotUnique
           retry
         end
       end
@@ -75,7 +76,8 @@ module Land
           maybe_set_visit_attribution_from_pageview if controller.request.path =~ PAGEVIEW_ENDPOINT_REGEX
 
           @visit&.save! if @visit&.changed?
-        rescue ActiveRecord::RecordNotUnique
+        rescue ActiveRecord::RecordInvalid,
+               ActiveRecord::RecordNotUnique
           retry
         end
 
--- spec/land/trackers/api_tracker_spec.rb
+++ spec/land/trackers/api_tracker_spec.rb
@@ -280,7 +280,7 @@ RSpec.describe 'Land::Trackers::ApiTracker', type: :request do
     let(:cookie_id) { SecureRandom.uuid }
     let(:visit_id) { SecureRandom.uuid }
 
-    it 'record visit does not throw an error' do
+    it 'record visit rescues and retries on ActiveRecord::RecordNotUnique error' do
       allow(Land::Visit).to receive(:create)
         .and_raise(ActiveRecord::RecordNotUnique, 'Visit already exists')
         .and_call_original
@@ -296,10 +296,43 @@ RSpec.describe 'Land::Trackers::ApiTracker', type: :request do
                                             as: :json
     end
 
-    it 'load does not throw error on cookie race condition' do
+    it 'record visit rescues and retries on ActiveRecord::RecordInvalid error' do
+      allow(Land::Visit).to receive(:create)
+        .and_raise(ActiveRecord::RecordInvalid, 'Visit already exists')
+        .and_call_original
+
+      # this is asserting that the `record_visit` call does not throw an error,
+      # as this method that is called after the `record_visit` call
+      expect_any_instance_of(Land::Trackers::ApiTracker)
+        .to receive(:maybe_set_raw_query_string)
+        .and_call_original
+
+      # visit call
+      post "/api/v1/visit?#{query_string}", params: body,
+                                            as: :json
+    end
+
+    it 'load does not throw error on ActiveRecord::RecordNotUnique error' do
       allow(Land::Cookie)
         .to receive_message_chain(:where, :first_or_create)
         .and_raise(ActiveRecord::RecordNotUnique, 'Cookie already exists')
+        .and_return([])
+
+      # this is asserting that the `load` call does not throw an error,
+      # as this method that is called after the `load` call
+      expect_any_instance_of(Land::Trackers::ApiTracker)
+        .to receive(:record_visit)
+        .and_call_original
+
+      # visit call
+      post "/api/v1/visit?#{query_string}", params: body,
+                                            as: :json
+    end
+
+    it 'load does not throw error on ActiveRecord::RecordInvalid error' do
+      allow(Land::Cookie)
+        .to receive_message_chain(:where, :first_or_create)
+        .and_raise(ActiveRecord::RecordInvalid, 'Cookie already exists')
         .and_return([])
 
       # this is asserting that the `load` call does not throw an error,
