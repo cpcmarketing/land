6c14261ee224e355442a1650f2928bece9f14713
Adam Moffatt <adam@themoffatt.com>
2025-10-03 13:36:58 -0500

    adds logging the tracker type to datadog

--- app/models/land/visit.rb
+++ app/models/land/visit.rb
@@ -14,6 +14,15 @@ module Land
 
     validates :visit_id, presence: true, uniqueness: true
 
+    before_save do
+      tracker_class = caller.find{ |l| l.include?("Tracker") }&.split(' ')&.last&.split('#')&.first
+      # Attempt to add metadata to the active Datadog span if it exists.
+      Datadog::Tracing.active_span&.set_tag('visit_save', tracker_class:) if defined?(Datadog::Tracing) && Datadog::Tracing.respond_to?(:active_span)
+    rescue StandardError => e
+      # Rescue everything to avoid breaking the save.
+      nil
+    end
+
     after_initialize do
       self.id ||= SecureRandom.uuid
     end
